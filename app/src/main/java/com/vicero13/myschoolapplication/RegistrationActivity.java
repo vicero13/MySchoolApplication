6768c9c9c28aff95c290a78aae68181642388f3e
Robert Beigel <rbeygel@gmail.com>
2020-11-12 20:30:03 +0300

    add registration activity

--- app/src/main/java/com/vicero13/myschoolapplication/RegistrationActivity.java
+++ app/src/main/java/com/vicero13/myschoolapplication/RegistrationActivity.java
@@ -1,4 +1,14 @@
 package com.vicero13.myschoolapplication;
 
-public class RegistrationActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegistrationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+
 }
